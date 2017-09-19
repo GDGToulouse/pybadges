9d13192fad302f55ff143bb86ff5ebc7cf204b02
Maxime Pawlak <contact@maximepawlak.fr>
2017-09-19 16:59:07 +0200

    Move output files to out folder.

--- devfest.sh
+++ devfest.sh
@@ -1,10 +1,11 @@
  #!/bin/bash
+mkdir out
 
 draw_badges() {
     echo "Drawing badges $1 ..."
-    python ./pybadges.py -i badges-$1.csv -o badges-$1.pdf -b badges-$1.png
-    python ./download-qrcodes.py -i badges-$1.csv -o ./qrcodes
-    python ./pybadges_back.py -i badges-$1.csv -o badges-$1-back.pdf -b badges-back.png -q ./qrcodes
+    python ./pybadges.py -i badges-$1.csv -o ./out/badges-$1.pdf -b badges-$1.png
+    python ./download-qrcodes.py -i badges-$1.csv -o ./out/qrcodes
+    python ./pybadges_back.py -i badges-$1.csv -o ./out/badges-$1-back.pdf -b badges-back.png -q ./out/qrcodes
     echo
 }
 
